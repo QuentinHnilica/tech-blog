756fa42f6a52fcd8702debba17908f1feac40d48
unknown <Quentinhnilica@gmail.com>
2021-12-10 03:26:23 -0500

    signup Bug?

--- controllers/index.js
+++ controllers/index.js
@@ -62,7 +62,7 @@ router.post('/logout', (req, res) => {
 router.get('/signup', async(req, res) => {
     res.render('signUp',{
         logged_in: req.session.logged_in,
-        username: req.session.username,
+        //username: req.session.username,
     })
 });
 
